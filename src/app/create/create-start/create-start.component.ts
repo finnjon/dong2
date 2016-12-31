aa14a254127cbed11c9e4435534ec4bd4ab4fe0c
Finnjon <jonathondsmartin@gmail.com>
2016-12-31 18:33:49 +0000

    change default message when editing droplet

--- src/app/create/create-start/create-start.component.ts
+++ src/app/create/create-start/create-start.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { DropletService } from '../../droplet.service';
+import { Droplet } from '../../droplet';
 
 @Component({
   selector: 'app-create-start',
   template: `
-    <div>
+    <div *ngIf="newDroplet" class="pad8">
       Hi! Let's get started creating a droplet. The first thing to do is check that the droplet you want to create is not already in use. To do that, click here:
     </div>
-    <button class="btn btn-large" [routerLink]="['create1']">Start</button>
+    <div *ngIf="!newDroplet" class="pad8">
+      To edit your droplet simply click on the element you wish to edit on the right. To add something new click above.
+    </div>
+    <button *ngIf="newDroplet" class="btn btn-large" [routerLink]="['create1']">Start</button>
   `,
   styles: []
 })
 export class CreateStartComponent implements OnInit {
+  droplet: Droplet;
+  newDroplet = true;
 
-  constructor() { }
+  constructor(private dropletService: DropletService) {}
 
   ngOnInit() {
+    this.droplet = this.dropletService.getCurrentDroplet();
+    if (this.droplet._id){ this.newDroplet = false; }
   }
 
 }
