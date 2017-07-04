0a6dfa167e4c3524fc336b12f834a90a22afaa01
Finnjon <jonathondsmartin@gmail.com>
2017-07-04 09:10:09 +0300

    add ability to remove comments

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -58,6 +58,22 @@ export class EditorComponent implements OnInit {
       );
   }
 
+  removeComment(multi, field, index) {
+    if (!multi) { this.droplet.comments[field] = ""; }
+    else { this.droplet[field][index].comment = ""; }
+
+    this.httpService.saveDroplet(this.droplet)
+      .subscribe(
+        (droplet: Droplet) => {
+          this.dropletService.updateCurrentDroplet(droplet);
+        },
+        (error) => {
+          this.error = error;
+          this.flashMessagesService.show('An error occurred', { cssClass: 'alert-success', timeout: 2000 });
+        }
+      );
+  }
+
   //Notes on toggling. In order to toggle visibility we use the field and the index, which are passed in the click handler. By adding them together we can create a variable that combines them to provide a unique id for the field, even if there are many. This is what is toggled.
   toggleMe(field, index) {
     if (this.showEdit !== field + index) {
