69e5b927950f2611f7d593f2d3ce2e23ef269f43
Finnjon <jonathondsmartin@gmail.com>
2017-07-14 11:12:00 +0300

    minor bug fixes

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -44,7 +44,6 @@ export class HttpService {
     if (droplet._id) {
       droplet.updated_at = new Date().toJSON();
       droplet.status = this.checkStatus(droplet);
-      console.log(droplet.status);
       return this.put(droplet);
     }
     return this.post(droplet);
