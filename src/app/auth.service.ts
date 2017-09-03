ca5a873ac757ecd784e13b48371b693a31db47d7
Finnjon <jonathondsmartin@gmail.com>
2017-09-03 18:21:06 +0300

    change length of auth

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -41,7 +41,7 @@ export class Auth {
 
   private setSession(authResult): void {
     // Set the time that the access token will expire at
-    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    const expiresAt = JSON.stringify((authResult.expiresIn * 10000) + new Date().getTime());
     localStorage.setItem('access_token', authResult.accessToken);
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
